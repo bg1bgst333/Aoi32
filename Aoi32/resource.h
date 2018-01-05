56902f9d363fec8b813d575c099fe2c47c36c382
bg1bgst333 <bg1bgst333@gmail.com>
2018-01-05 10:58:03 +0900

    -メニューアイテム"文字コード"とサブメニューアイテム"Shift_JIS", "Unicode"の追加.

--- Aoi32/resource.h
+++ Aoi32/resource.h
@@ -5,13 +5,21 @@
 #define IDR_MENU1                       101
 #define ID_FILE_OPEN                    40001
 #define ID_FILE_SAVE_AS                 40002
+#define ID_40003                        40003
+#define ID_ENCODE_CHARSET_ENCODE        40004
+#define ID_ENC_CHARSET_ENCODE           40005
+#define ID_40006                        40006
+#define ID_ENC_SHIFT_JIS                40007
+#define ID_40008                        40008
+#define ID_ENC_UTF_16                   40009
+#define ID_ENC_UNICODE                  40010
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40003
+#define _APS_NEXT_COMMAND_VALUE         40011
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
