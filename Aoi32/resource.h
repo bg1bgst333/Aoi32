39b26b0a1ba5dec48854f7238776030b023e173b
bg1bgst333 <bg1bgst333@gmail.com>
2018-07-13 15:27:11 +0900

    -改行コードのメニューアイテムを追加.

--- Aoi32/resource.h
+++ Aoi32/resource.h
@@ -13,13 +13,22 @@
 #define ID_40008                        40008
 #define ID_ENC_UTF_16                   40009
 #define ID_ENC_UNICODE                  40010
+#define ID_40011                        40011
+#define ID_40012                        40012
+#define ID_40013                        40013
+#define ID_40014                        40014
+#define ID_40015                        40015
+#define ID_LINE_CRLF                    40016
+#define ID_LINE_LF                      40017
+#define ID_ID_LINE_CR                   40018
+#define ID_LINE_CR                      40019
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        102
-#define _APS_NEXT_COMMAND_VALUE         40011
+#define _APS_NEXT_COMMAND_VALUE         40020
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
